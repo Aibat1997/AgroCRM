77b929be6489917be0a44a761feb6cb60f2396c8
Aibat1997 <aibatkarashev>
2025-11-30 22:58:22 +0500

    finish CRUD of warehouse

--- app/Http/Controllers/Api/WarehouseController.php
+++ app/Http/Controllers/Api/WarehouseController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Warehouse\StoreWarehouseRequest;
+use App\Http\Requests\Api\Warehouse\UpdateWarehouseRequest;
 use App\Http\Resources\WarehouseResource;
 use App\Models\Warehouse;
 use Illuminate\Http\Request;
@@ -13,5 +15,38 @@ class WarehouseController extends Controller
     {
         $warehouses = Warehouse::with('company')->filter($request->all())->get();
         return $this->return_success(WarehouseResource::collection($warehouses));
+    }
+
+    public function store(StoreWarehouseRequest $request)
+    {
+        $warehouse = Warehouse::create($request->validated());
+        $warehouse->load('company');
+
+        return $this->return_success(new WarehouseResource($warehouse));
+    }
+
+    public function show(Warehouse $warehouse)
+    {
+        $warehouse->load('company');
+        return $this->return_success(new WarehouseResource($warehouse));
+    }
+
+    public function update(UpdateWarehouseRequest $request, Warehouse $warehouse)
+    {
+        $warehouse->update($request->validated());
+        $warehouse->load('company');
+
+        return $this->return_success(new WarehouseResource($warehouse));
+    }
+
+    public function destroy(Warehouse $warehouse)
+    {
+        $warehouse->delete();
+        return $this->return_success();
+    }
+
+    public function restore(Warehouse $warehouse)
+    {
+        //
     }
 }
