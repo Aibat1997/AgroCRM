41ee28e4fdecee02d75fced77e7c588bc3476a3b
Aibat1997 <aibatkarashev@gmail.com>
2026-01-20 15:55:57 +0500

    update Uploader

--- app/FileSystem/Uploader.php
+++ app/FileSystem/Uploader.php
@@ -7,28 +7,32 @@ use Illuminate\Support\Facades\Storage;
 
 abstract class Uploader
 {
-    protected $file;
-    private $path;
-    private $original_name;
-    protected $new_file_name;
-    private $extension;
-    private $size;
-    private $disk;
+    protected UploadedFile $file;
+    protected string $path;
+    protected string $originalName;
+    protected ?string $newFileName;
+    protected string $extension;
+    protected int $size;
+    protected string $disk;
 
-    public function __construct(UploadedFile $file, string $path, ?string $new_file_name = null, ?string $disk = 'public')
-    {
+    public function __construct(
+        UploadedFile $file,
+        string $path,
+        ?string $newFileName = null,
+        string $disk = 'public'
+    ) {
         $this->file = $file;
-        $this->path = str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $path);
-        $this->original_name = $file->getClientOriginalName();
+        $this->path = trim($path, '/\\');
+        $this->originalName = $file->getClientOriginalName();
         $this->extension = $file->extension();
         $this->size = $file->getSize();
-        $this->new_file_name = $this->normalizeFileName($new_file_name);
+        $this->newFileName = $this->normalizeFileName($newFileName);
         $this->disk = $disk;
     }
 
     public function getFileName(): string
     {
-        return $this->original_name;
+        return $this->originalName;
     }
 
     public function getExtension(): string
@@ -44,9 +48,13 @@ abstract class Uploader
     public function getFormattedSize(int $precision = 2): string
     {
         $units = ['B', 'KB', 'MB', 'GB', 'TB'];
-
         $bytes = max($this->size, 0);
-        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+
+        if ($bytes === 0) {
+            return '0 B';
+        }
+
+        $pow = floor(log($bytes) / log(1024));
         $pow = min($pow, count($units) - 1);
 
         $bytes /= pow(1024, $pow);
@@ -56,26 +64,23 @@ abstract class Uploader
 
     public function upload(): string
     {
-        if ($this->new_file_name) {
-            $stored_path = $this->file->storeAs($this->path, $this->new_file_name, $this->disk);
-        } else {
-            $stored_path = $this->file->store($this->path, $this->disk);
-        }
+        $storedPath = $this->newFileName
+            ? $this->file->storeAs($this->path, $this->newFileName, $this->disk)
+            : $this->file->store($this->path, $this->disk);
 
-        return Storage::url(str_replace('\\', '/', $stored_path));
+        return Storage::url($storedPath);
     }
 
-    private function normalizeFileName(?string $new_file_name): ?string
+    private function normalizeFileName(?string $newFileName): ?string
     {
-        if ($new_file_name) {
-            $info = pathinfo($new_file_name);
-            if (isset($info['extension'])) {
-                return $new_file_name;
-            } else {
-                return "{$new_file_name}.{$this->extension}";
-            }
-        } else {
+        if (!$newFileName) {
             return null;
         }
+
+        $pathInfo = pathinfo($newFileName);
+
+        return isset($pathInfo['extension'])
+            ? $newFileName
+            : "{$newFileName}.{$this->extension}";
     }
 }
