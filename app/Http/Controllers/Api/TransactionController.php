921f3fa621cf0a893c4aac367b571e13930599a4
Aibat1997 <aibatkarashev@gmail.com>
2026-02-09 17:19:50 +0500

    add TransactionController

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -2,22 +2,31 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\DTO\TransactionDTO;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Transaction\TransactionRequest;
 use App\Models\Transaction;
+use App\Services\TransactionService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TransactionController extends Controller
 {
-    public function __construct() {}
+    public function __construct(private readonly TransactionService $transactionService) {}
 
     public function index(Request $request)
     {
         //
     }
 
-    public function store(Request $request)
+    public function store(TransactionRequest $request)
     {
-        //
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+        $dto = TransactionDTO::fromArray($request->validated());
+        $transaction = $this->transactionService->store($dto, $user);
+
+        return response()->json($transaction, 201);
     }
 
     public function show(Transaction $transaction)
@@ -25,9 +34,12 @@ class TransactionController extends Controller
         //
     }
 
-    public function update(Request $request, Transaction $transaction)
+    public function update(TransactionRequest $request, Transaction $transaction)
     {
-        //
+        $dto = TransactionDTO::fromArray($request->validated());
+        $this->transactionService->update($dto, $transaction);
+
+        return response()->json($transaction, 200);
     }
 
     public function destroy(Transaction $transaction)
