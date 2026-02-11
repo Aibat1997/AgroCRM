61d40caa15f5e6d998adb95cd6e0346e2059bc99
Aibat1997 <aibatkarashev@gmail.com>
2026-02-11 14:58:44 +0500

    update TransactionFormFieldController

--- app/Http/Controllers/Api/TransactionFormFieldController.php
+++ app/Http/Controllers/Api/TransactionFormFieldController.php
@@ -4,18 +4,20 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\TransactionFormFieldResource;
-use App\Models\TransactionType;
+use App\Services\TransactionFormFieldCacheService;
 use Illuminate\Http\Request;
 
 class TransactionFormFieldController extends Controller
 {
+    public function __construct(private readonly TransactionFormFieldCacheService $transactionFormFieldCacheService) {}
+
     public function index(Request $request)
     {
         $request->validate([
             'transaction_type_id' => 'required|integer|exists:transaction_types,id',
         ]);
 
-        $transactionType = TransactionType::with('formFields')->find($request->input('transaction_type_id'));
-        return $this->return_success(TransactionFormFieldResource::collection($transactionType->formFields));
+        $transactionFormFields = $this->transactionFormFieldCacheService->getTransactionFormFieldByTypeId($request->input('transaction_type_id'));
+        return $this->return_success(TransactionFormFieldResource::collection($transactionFormFields));
     }
 }
