aeefbe8a4e727e5a4f0a423f6de50731f19aa773
Aibat1997 <aibatkarashev@gmail.com>
2026-02-12 19:16:59 +0500

    fix api.php

--- routes/api.php
+++ routes/api.php
@@ -33,6 +33,7 @@ Route::name('api.')->group(function () {
     Route::post('register', [AuthorizationController::class, 'register'])->middleware('throttle:3,1');
     Route::post('login', [AuthorizationController::class, 'login'])->middleware('throttle:3,1');
 
+    Route::get('companies', [CompanyController::class, 'index']);
     Route::get('registration-roles', [RoleController::class, 'registrationRoles']);
 
     Route::middleware(['auth:api'])->group(function () {
@@ -40,7 +41,6 @@ Route::name('api.')->group(function () {
         Route::get('units', [UnitController::class, 'index']);
         Route::get('currencies', [CurrencyController::class, 'index']);
         Route::get('real-estate-types', [RealEstateTypeController::class, 'index']);
-        Route::get('companies', [CompanyController::class, 'index']);
         Route::get('payment-frequencies', [PaymentFrequencyController::class, 'index']);
         Route::get('cotton-purchase-price', [CottonPurchasePriceController::class, 'index']);
         Route::get('transaction-types', [TransactionTypeController::class, 'index']);
